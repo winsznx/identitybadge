e3e662b6d517c68c2dd92a491d6fc38a141ef407
winsznx <timjosh507@gmail.com>
2026-02-08 16:22:52 +0100

    refactor: rename contracts to match project name and update references

--- contracts/base/script/DeployUidentitybadge.s.sol
+++ contracts/base/script/DeployUidentitybadge.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/ChainRegistry.sol";
+import "../src/Uidentitybadge.sol";
 
 contract DeployScript is Script {
     function run() external {
@@ -10,9 +10,9 @@ contract DeployScript is Script {
         
         vm.startBroadcast(deployerPrivateKey);
         
-        ChainRegistry registry = new ChainRegistry();
+        Uidentitybadge registry = new Uidentitybadge();
         
-        console.log("ChainRegistry deployed to:", address(registry));
+        console.log("Uidentitybadge deployed to:", address(registry));
         
         vm.stopBroadcast();
     }
