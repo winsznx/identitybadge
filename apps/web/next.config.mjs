3d98f0e9ca91321b5d71034401239f5e552147a6
winsznx <timjosh507@gmail.com>
2026-02-27 21:11:15 +0100

    Update apps/web/next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    transpilePackages: ['@chainregistry/shared', '@chainregistry/base-adapter', '@chainregistry/stacks-adapter'],
+    transpilePackages: ['@identitybadge/shared', '@identitybadge/base-adapter', '@identitybadge/stacks-adapter'],
 };
 
 export default nextConfig;
